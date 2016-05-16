0966b9af264320d5001c70c1641d95ef139bc158
Istvan Cebrian <istvan.cebrian@gmail.com>
2016-05-16 21:00:47 +0100

    add phphost

---
+++ phphost/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="styles.css" rel="stylesheet" />
+    <title>Hello World!</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    
+</head>
+<body>
+
+    <div id="container">
+        <div id="sep">Hello World!</div>
+        <div id="menu">Brought to you by: <b><?php echo gethostname(); ?></b></div>
+    </div>
+
+
+</body>
+</html>
